c6095ae15fefc68b7cdf590dc6ad133b96f9c428
ss925336-hash <ss925336@gmail.com>
2026-06-08 15:56:39 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'gym-tracker-v3.1';
-const urlsToCache = ['/gym-tracker/', '/gym-tracker/index.html'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).then(r => { const c = r.clone(); caches.open(CACHE_NAME).then(ca => ca.put(e.request, c)); return r; }).catch(() => caches.match(e.request))); });
